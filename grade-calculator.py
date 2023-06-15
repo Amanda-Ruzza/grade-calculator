a7874d2612694a5c7a91eb2de663d1388039b0e3
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2023-06-15 11:44:34 -0400

    Add added_scores list + loop

--- grade-calculator.py
+++ grade-calculator.py
@@ -9,19 +9,30 @@ grade_c = "C"
 grade_b = "B"
 grade_a = "A"
 
+# add items from test_scores_processing to added_scores list
+def add_items_to_list():
+    added_scores = [] 
+    for item in test_scores_processing:
+        added_scores.append(item)
+        print(f"This is the added scores list: {added_scores}")
+
 
 def calculator():
     try:
         test_scores_input = int(input("Please enter the test result score:\n"))   
-
+        
         if test_scores_input <= 100 or  0:
             test_scores_processing = [test_scores_input]
+            
             grade_result = [grade_f if num < 61 else grade_d if num < 70 
                             else grade_c if num < 80 else grade_b if num < 90 
                             else grade_a if num < 101 else None 
                             for num in test_scores_processing ]    
             print(f"The grade is {grade_result}")
             print(f"This is the test score processing list: {test_scores_processing}")
+            add_items_to_list()
+
+            
                        
         elif test_scores_input > 100:
             raise ValueError(f"I can only process numbers from '0 - 100'. {test_scores_input} is greater than 100!")
@@ -39,9 +50,9 @@ def calculator():
 for i in range(5):
     calculator()
     print(f"Testing iteration number:  {i+1}\n")
-    added_scores = test_scores_processing.append(test_scores_processing)  
-    print(f"This is the added scores list: {added_scores}")
+    
 
+   
 print("The average grade of the 5 grades is: \n")
 
 sys.exit("This processing job is concluded, goodbye!\n")
